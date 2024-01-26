5f525de52e596c5cb10dd8f5925e0b8d73e449b0
Aymen TROUDI <aimenux@gmail.com>
2024-01-26 14:24:09 +0100

    Upgrade to support .net 8

---
+++ App/LatestBenchConfig.cs
@@ -0,0 +1,37 @@
+﻿using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Exporters.Csv;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Order;
+using BenchmarkDotNet.Reports;
+
+namespace App;
+
+public class LatestBenchConfig : ManualConfig
+{
+    public LatestBenchConfig()
+    {
+        HideColumns(Column.RatioSD);
+        AddColumn(RankColumn.Arabic);
+        AddColumn(StatisticColumn.Min);
+        AddColumn(StatisticColumn.Max);
+        AddColumn(CategoriesColumn.Default);
+        AddColumnProvider(DefaultColumnProviders.Instance);
+        AddLogger(ConsoleLogger.Default);
+        AddExporter(RPlotExporter.Default);
+        AddExporter(CsvMeasurementsExporter.Default);
+        AddExporter(HtmlExporter.Default);
+        AddExporter(MarkdownExporter.GitHub);
+        AddExporter(MarkdownExporter.Default);
+        AddDiagnoser(MemoryDiagnoser.Default);
+        AddDiagnoser(ExceptionDiagnoser.Default);
+        AddLogicalGroupRules(BenchmarkLogicalGroupRule.ByParams);
+        WithOrderer(new DefaultOrderer(SummaryOrderPolicy.FastestToSlowest));
+        WithSummaryStyle(SummaryStyle.Default.WithRatioStyle(RatioStyle.Trend));
+        AddJob(Job.Default.WithRuntime(CoreRuntime.Core80).WithId(nameof(RuntimeMoniker.Net80)));
+    }
+}
---
+++ App/TasksBench.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using BenchmarkDotNet.Attributes;
+
+namespace App;
+
+[Config(typeof(LatestBenchConfig))]
+[BenchmarkCategory(nameof(BenchCategory.Default))]
+public class TasksBench
+{
+    [Params(10, 100, 500)] 
+    public int Delay { get; set; }
+    
+    [Benchmark]
+    public async Task WithTryCatch()
+    {
+        try
+        {
+            await Task.Delay(Delay);
+        }
+        catch(Exception ex)
+        {
+            Debug.WriteLine(ex);
+        }
+    }
+
+    [Benchmark]
+    public async Task WithoutTryCatch()
+    {
+        await Task.Delay(Delay);
+    }
+}
